2a1b626dddcb27e8240e80bb55d5711aac20b2b4
faisalhatila <hatilas1994@gmail.com>
2021-04-16 18:07:18 +0500

    dice app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,77 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // leftDiceNumber = 4;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("Left button got pressed");
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print("Right button got pressed");
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          )
+        ],
+      ),
+    );
   }
 }
+
+// class DicePage extends StatelessWidget {
+//   int leftDiceNumber = 5;
+//   @override
+//   Widget build(BuildContext context) {
+//     leftDiceNumber = 4;
+//     return Center(
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             child: FlatButton(
+//               onPressed: () {
+//                 print("Left button got pressed");
+//               },
+//               child: Image.asset('images/dice$leftDiceNumber.png'),
+//             ),
+//           ),
+//           Expanded(
+//             child: FlatButton(
+//                 onPressed: () {
+//                   print("Right button got pressed");
+//                 },
+//                 child: Image.asset('images/dice2.png')),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
